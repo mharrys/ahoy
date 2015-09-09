a5068d1c161e267210bf9eebd0d109e5b44ca9a7
Lucas Åström <lucasas@kth.se>
2015-09-09 13:38:27 +0200

    remove ASCII art

--- src/skur_app.erl
+++ src/skur_app.erl
@@ -1,17 +1,9 @@
 -module(skur_app).
-
 -behaviour(application).
-
-%% Application callbacks
 -export([start/2, stop/1]).
-
 -ifdef(TEST).
 -include_lib("eunit/include/eunit.hrl").
 -endif.
-
-%% ===================================================================
-%% Application callbacks
-%% ===================================================================
 
 start(_StartType, _StartArgs) ->
     skur_sup:start_link().
