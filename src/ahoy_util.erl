cffc6e4f767a438fef14f3dc5750ddba5dbc47b7
Mattias Harrysson <mathar@kth.se>
2017-02-05 19:07:25 +0100

    remove request_url

--- src/ahoy_util.erl
+++ src/ahoy_util.erl
@@ -1,23 +1,7 @@
 -module(ahoy_util).
 
--export([request_url/2,
-         encode_url/1]).
+-export([encode_url/1]).
 
-%% Creates a URL from a set of parameters so that {key, val} becomes &key=val
-%% with URL encoding
-request_url(Url, Parameters) ->
-    request_url(Url, Parameters, "").
-request_url(Url, [], Acc) ->
-    Url ++ "?" ++ Acc;
-request_url(Url, [{Key, Value}|Parameters], Acc) when is_integer(Value) ->
-    NewAcc = Acc ++ http_uri:encode(Key) ++ "=" ++ integer_to_list(Value) ++ "&",
-    request_url(Url, Parameters, NewAcc);
-request_url(Url, [{Key, Value}|Parameters], Acc) when is_binary(Value) ->
-    NewAcc = Acc ++ http_uri:encode(Key) ++ "=" ++ encode_url(Value) ++ "&",
-    request_url(Url, Parameters, NewAcc);
-request_url(Url, [{Key, Value}|Parameters], Acc) ->
-    NewAcc = Acc ++ http_uri:encode(Key) ++ "=" ++ http_uri:encode(Value) ++ "&",
-    request_url(Url, Parameters, NewAcc).
 
 %% URL encodes binary data according to RFC1738 only needed because
 %% http_uri:encode can't handle binary
