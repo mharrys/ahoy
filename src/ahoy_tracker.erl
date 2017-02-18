01fdde66051f629e20e10a0e3140349c29a09fdd
Mattias Harrysson <mathar@kth.se>
2017-02-18 13:12:51 +0100

    tracker now sends new peers once received

--- src/ahoy_tracker.erl
+++ src/ahoy_tracker.erl
@@ -1,9 +1,7 @@
 -module(ahoy_tracker).
 
--export([start_link/2,
-         start_link/3,
-         get_last_response/1,
-         get_peers/1]).
+-export([start_link/3,
+         start_link/4]).
 
 -behaviour(gen_server).
 
@@ -18,50 +16,41 @@
 -include_lib("ahoy_tracker_progress.hrl").
 -include_lib("ahoy_tracker_response.hrl").
 
--record(state, {meta :: metainfo(),
-                port :: port_number(),
-                progress :: tracker_progress(),
-                response :: tracker_response()}).
+-type torrent() :: pid().
 
-start_link(Meta, Port) ->
+-record(state, {torrent :: torrent(),
+                meta :: metainfo(),
+                port :: port_number(),
+                progress :: tracker_progress()}).
+
+start_link(Torrent, Meta, Port) ->
     Info = Meta#metainfo.info,
     Progress = #tracker_progress{
         uploaded = 0,
         downloaded = 0,
         left = Info#info.length
     },
-    gen_server:start_link(?MODULE, [Meta, Port, Progress], []).
+    gen_server:start_link(?MODULE, [Torrent, Meta, Port, Progress], []).
 
-start_link(Meta, Port, Progress) ->
-    gen_server:start_link(?MODULE, [Meta, Port, Progress], []).
+start_link(Torrent, Meta, Port, Progress) ->
+    gen_server:start_link(?MODULE, [Torrent, Meta, Port, Progress], []).
 
-%% @doc Return latest response recieved from tracker.
--spec get_last_response(pid()) -> tracker_response().
-get_last_response(Pid) ->
-    gen_server:call(Pid, get_last_response).
-
-%% @doc Return peers from latest response recieved from tracker.
--spec get_peers(pid()) -> list(peer_address()).
-get_peers(Pid) ->
-    gen_server:call(Pid, get_peers).
-
-init([Meta, Port, Progress]) ->
-    State = #state{meta = Meta, port = Port, progress = Progress},
+init([Torrent, Meta, Port, Progress]) ->
+    State = #state{torrent=Torrent, meta=Meta, port=Port, progress=Progress},
     gen_server:cast(self(), update),
     {ok, State}.
 
-handle_call(get_last_response, _From, State=#state{response=Resp}) ->
-    {reply, Resp, State};
-handle_call(get_peers, _From, State=#state{response=Resp}) ->
-    {reply, Resp#tracker_response.peers, State};
 handle_call(_Request, _From, State) ->
     {reply, {error, unknown_call}, State}.
 
-handle_cast(update, State=#state{meta=Meta, port=Port, progress=Progress}) ->
-    Resp = request_peers(Meta, Port, Progress),
+handle_cast(update, State=#state{torrent=Torrent, meta=Meta, port=Port,
+        progress=Progress}) ->
+    Resp = request_update(Meta, Port, Progress),
     Interval = Resp#tracker_response.interval,
-    timer:apply_after(Interval * 1000, gen_server, cast, [self(), update]),
-    {noreply, State#state{response = Resp}};
+    update(Interval),
+    PeerAddresses = Resp#tracker_response.peer_addresses,
+    ahoy_torrent:update_tracker_peers(Torrent, PeerAddresses),
+    {noreply, State};
 handle_cast(_Msg, State) ->
     {noreply, State}.
 
@@ -74,8 +63,12 @@ terminate(_Reason, _State) ->
 code_change(_OldVsn, State, _Extra) ->
     {ok, State}.
 
-%% @doc Request tracker to provide a list of active peers.
-request_peers(Meta, Port, Progress) ->
+%% Call update after specified number of seconds.
+update(Interval) ->
+    timer:apply_after(Interval * 1000, gen_server, cast, [self(), update]).
+
+%% Request update from tracker.
+request_update(Meta, Port, Progress) ->
     Url = ahoy_tracker_message:encode_request(Meta, Port, Progress),
     {ok, {_, _, Body}} = httpc:request(lists:flatten(Url)),
     ahoy_tracker_message:decode_response(Body).
